f99b46b11fae524e1bc01e29ea26c05f362dc8fc
Francesco Sergiano <f.sergiano@demonet.it>
2025-07-22 01:43:17 +0200

    Inizio sviluppo web-app anche per mobile

--- ProgettoTecnologieWebFrancescoSergiano/ProgettoTecnologieWebFrancescoSergiano/settings.py
+++ ProgettoTecnologieWebFrancescoSergiano/ProgettoTecnologieWebFrancescoSergiano/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4*)#rq0j@j0tc=@p3h7clu&3exu2adli4xje%iah09zmzz9nf6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
